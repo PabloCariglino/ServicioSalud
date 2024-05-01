d903bc98f5968096f5cd71f89e9377554a714780
Pablo Cariglino <pablocariglino93@gmail.com>
2024-05-01 17:42:29 -0300

    1-5-24

---
+++ ServicioSalud/src/test/java/com/SaludWeb/ServicioSalud/ServicioSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SaludWeb.ServicioSalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioSaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
